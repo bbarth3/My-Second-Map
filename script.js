846e0e662d27a7f80625fda38493f4d1629259a7
Brendan Barth <98061278+bbarth3@users.noreply.github.com>
2022-01-19 17:04:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
+        const map = new Map({
+          basemap: "streets-night-vector"
+        });
+
+        const view = new MapView({
+          container: "viewDiv",
+          map: map,
+          zoom: 4,
+          center: [-88.31, 41.85] // longitude, latitude
+        });
+      });
